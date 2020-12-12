a3802bd722c817a0787e07769716dc37873647d0
Bianka Gieda <bianka.gieda@stud.uni-corvinus.hu>
2020-12-12 20:10:03 +0100

    Betűmátrix létrehozása és a szavak betöltése csv fájlból.

---
+++ IRF_Project_Gieda_Bianka_FYZINS/IRF_Project_Gieda_Bianka_FYZINS/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace IRF_Project_Gieda_Bianka_FYZINS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_betuk = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel_betuk
+            // 
+            this.panel_betuk.Location = new System.Drawing.Point(46, 181);
+            this.panel_betuk.Name = "panel_betuk";
+            this.panel_betuk.Size = new System.Drawing.Size(300, 200);
+            this.panel_betuk.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 408);
+            this.Controls.Add(this.panel_betuk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_betuk;
+    }
+}
+
---
+++ IRF_Project_Gieda_Bianka_FYZINS/IRF_Project_Gieda_Bianka_FYZINS/Form1.cs
@@ -0,0 +1,64 @@
+﻿using IRF_Project_Gieda_Bianka_FYZINS.Osztalyok;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_Project_Gieda_Bianka_FYZINS
+{
+    public partial class Form1 : Form
+    {
+        string[] szavak;
+
+        public Form1()
+        {
+            InitializeComponent();
+            BetuMatrix();
+            SzavakBetoltese();
+            
+        }
+
+        private void BetuMatrix()
+        {
+            int tavolsag = 2;
+            string[] felirat = new string[35] {"A","Á","B","C","D","E","F","G","H","I","Í","J","K","L","J","M","N","O","Ó","Ö","Ő","P","Q","R","S","T","U","Ú","Ü","Ű","V","W","X","Y","Z"};
+            int i=0;
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    Betuk b = new Betuk();
+                    b.Left = col * b.Width + (int)(Math.Floor((double)(col))) * tavolsag;
+                    b.Top = row * b.Height + (int)(Math.Floor((double)(row))) * tavolsag;
+                    panel_betuk.Controls.Add(b);
+
+                    b.Text = (string)(felirat[i]);
+                    i++;
+                }
+            }
+        }
+
+        private void SzavakBetoltese()
+        {
+            using (StreamReader szo = new StreamReader("szavak.csv", Encoding.Default))
+            {
+                while (!szo.EndOfStream)
+                {
+                    szavak = szo.ReadLine().Split(';');
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
